5acf5b4844c3854124bd5cd085a5122e349062ae
b3spectacled <modernbetamc@gmail.com>
2021-10-04 00:13:21 -0700

    Readd common underground and ocean structures.

--- src/main/java/com/bespectacled/modernbeta/world/biome/OldBiomeStructures.java
+++ src/main/java/com/bespectacled/modernbeta/world/biome/OldBiomeStructures.java
@@ -10,14 +10,27 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.gen.feature.ConfiguredStructureFeatures;
 
 public class OldBiomeStructures {
     private static RegistryKey<Biome> biomeKeyOf(Identifier id) {
         return RegistryKey.of(Registry.BIOME_KEY, id);
     }
     
+    private static void addDefaultUndergroundStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        consumer.accept(ConfiguredStructureFeatures.STRONGHOLD, biomeKeyOf(id));
+        consumer.accept(MixinConfiguredStructureFeaturesAccessor.getMineshaft(), biomeKeyOf(id));
+    }
+    
+    private static void addDefaultOceanStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        consumer.accept(MixinConfiguredStructureFeaturesAccessor.getShipwreck(), biomeKeyOf(id));
+        consumer.accept(MixinConfiguredStructureFeaturesAccessor.getMineshaft(), biomeKeyOf(id));
+    }
+    
     public static void addCommonStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
-        consumer.accept(MixinConfiguredStructureFeaturesAccessor.getVillagePlains(),biomeKeyOf(id));
+        addDefaultUndergroundStructures(consumer, id);
+        
+        consumer.accept(MixinConfiguredStructureFeaturesAccessor.getVillagePlains(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getPillagerOutpost(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortal(), biomeKeyOf(id));
     }
@@ -29,6 +42,8 @@ public class OldBiomeStructures {
             consumer.accept(MixinConfiguredStructureFeaturesAccessor.getOceanRuinCold(), biomeKeyOf(id));
         }
         
+        addDefaultOceanStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getBuriedTreasure(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortalOcean(), biomeKeyOf(id));
         consumer.accept(OldStructures.CONF_OCEAN_SHRINE_STRUCTURE, biomeKeyOf(id));
@@ -38,6 +53,8 @@ public class OldBiomeStructures {
         if (addVillages) {
             consumer.accept(MixinConfiguredStructureFeaturesAccessor.getVillageDesert(), biomeKeyOf(id));
         }
+
+        addDefaultUndergroundStructures(consumer, id);
         
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortalDesert(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getDesertPyramid(), biomeKeyOf(id));
@@ -45,36 +62,50 @@ public class OldBiomeStructures {
     }
     
     public static void addForestStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getMansion(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortal(), biomeKeyOf(id));
     }
     
     public static void addPlainsStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getPillagerOutpost(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortal(), biomeKeyOf(id));
     }
     
     public static void addRainforestStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getJunglePyramid(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortalJungle(), biomeKeyOf(id));
     }
     
     public static void addSeasonalForest(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortal(), biomeKeyOf(id));
     }
     
     public static void addLowlandStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getVillagePlains(),biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getPillagerOutpost(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortal(), biomeKeyOf(id));
     }
     
     public static void addSwamplandStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getSwampHut(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getRuinedPortalSwamp(), biomeKeyOf(id));
     }
     
     public static void addTaigaStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getVillageTaiga(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getIgloo(),biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getPillagerOutpost(), biomeKeyOf(id));
@@ -82,6 +113,8 @@ public class OldBiomeStructures {
     }
     
     public static void addTundraStructures(BiConsumer<ConfiguredStructureFeature<?, ?>, RegistryKey<Biome>> consumer, Identifier id) {
+        addDefaultUndergroundStructures(consumer, id);
+        
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getVillageSnowy(), biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getIgloo(),biomeKeyOf(id));
         consumer.accept(MixinConfiguredStructureFeaturesAccessor.getPillagerOutpost(), biomeKeyOf(id));
