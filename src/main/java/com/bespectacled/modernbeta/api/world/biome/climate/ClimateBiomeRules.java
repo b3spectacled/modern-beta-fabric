4c90d4a9045d5d6d0ddef5194cac90ecc3d1c395
b3spectacled <modernbetamc@gmail.com>
2021-11-03 20:24:34 -0700

    Combine ClimateBiomeRule classes.

--- src/main/java/com/bespectacled/modernbeta/api/world/biome/climate/ClimateBiomeRules.java
+++ src/main/java/com/bespectacled/modernbeta/api/world/biome/climate/ClimateBiomeRules.java
@@ -46,4 +46,25 @@ public class ClimateBiomeRules {
             return new ClimateBiomeRules(this.rules);
         }
     }
+    
+    private static class ClimateBiomeRule {
+        private final Predicate<Biome> rule;
+        private final Supplier<Clime> supplier;
+        
+        public ClimateBiomeRule(Predicate<Biome> rule, Supplier<Clime> supplier) {
+            this.rule = rule;
+            this.supplier = supplier;
+        }
+        
+        public Clime apply(Biome biome) {
+            if (this.test(biome))
+                return this.supplier.get();
+            
+            return null;
+        }
+        
+        private boolean test(Biome biome) {
+            return this.rule.test(biome);
+        }
+    }
 }
