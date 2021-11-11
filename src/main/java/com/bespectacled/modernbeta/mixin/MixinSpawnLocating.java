a7f05658d1f9b6e2554c1e9435c2997709fc9994
b3spectacled <modernbetamc@gmail.com>
2021-11-11 15:18:21 -0800

    Improved player spawn behavior; Vanilla behavior of moving player spawn to topmost solid block after spawn location is overridden, making in-cave spawns like with the gargamel seed possible again.

---
+++ src/main/java/com/bespectacled/modernbeta/mixin/MixinSpawnLocating.java
@@ -0,0 +1,31 @@
+package com.bespectacled.modernbeta.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.bespectacled.modernbeta.world.gen.OldChunkGenerator;
+
+import net.minecraft.server.network.SpawnLocating;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+
+@Mixin(SpawnLocating.class)
+public class MixinSpawnLocating {
+    /*
+     * Override vanilla behavior of moving player to highest solid block, 
+     * even after finding initial spawn point.
+     */
+    @Inject(method = "findOverworldSpawn", at = @At("HEAD"), cancellable = true)
+    private static void injectFindOverworldSpawn(ServerWorld world, int x, int z, CallbackInfoReturnable<BlockPos> info) {
+        ChunkGenerator chunkGenerator = world.getChunkManager().getChunkGenerator();
+        
+        if (chunkGenerator instanceof OldChunkGenerator oldChunkGenerator) {
+            int spawnY = world.getLevelProperties().getSpawnY();
+            
+            info.setReturnValue(new BlockPos(x, spawnY, z));
+        }
+    }
+}
