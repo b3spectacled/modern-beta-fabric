1d9bc06efebec247613064e5295308b4f6643135
b3spectacled <modernbetamc@gmail.com>
2021-12-05 18:26:17 -0800

    Add NbtListBuilder for compound builder.

--- src/main/java/com/bespectacled/modernbeta/util/NbtCompoundBuilder.java
+++ src/main/java/com/bespectacled/modernbeta/util/NbtCompoundBuilder.java
@@ -1,12 +1,19 @@
 package com.bespectacled.modernbeta.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 
 public class NbtCompoundBuilder {
     private final NbtCompound compound;
+    private final Map<String, NbtListBuilder> lists;
     
     public NbtCompoundBuilder() {
         this.compound = new NbtCompound();
+        this.lists = new HashMap<>();
     }
     
     public NbtCompoundBuilder putString(String key, String value) {
@@ -39,7 +46,24 @@ public class NbtCompoundBuilder {
         return this;
     }
     
+    public NbtCompoundBuilder addListItem(String key, NbtElement element) {
+        NbtListBuilder builder = this.lists.get(key);
+        
+        if (builder == null) {
+            builder = new NbtListBuilder();
+            this.lists.put(key, builder);
+        }
+        
+        builder.add(element);
+        
+        return this;
+    }
+    
     public NbtCompound build() {
+        for (Entry<String, NbtListBuilder> entry : this.lists.entrySet()) {
+            this.compound.put(entry.getKey(), entry.getValue().build());
+        }
+        
         return this.compound;
     }
 }
---
+++ src/main/java/com/bespectacled/modernbeta/util/NbtListBuilder.java
@@ -0,0 +1,25 @@
+package com.bespectacled.modernbeta.util;
+
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+
+public class NbtListBuilder {
+    private final NbtList list;
+    private int index;
+    
+    public NbtListBuilder() {
+        this.list = new NbtList();
+        this.index = 0;
+    }
+    
+    public NbtListBuilder add(NbtElement element) {
+        this.list.add(this.index, element);
+        index++;
+        
+        return this;
+    }
+    
+    public NbtList build() {
+        return this.list;
+    }
+}
