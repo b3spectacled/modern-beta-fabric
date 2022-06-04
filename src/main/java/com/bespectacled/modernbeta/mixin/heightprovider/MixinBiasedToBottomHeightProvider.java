4617adb31e8e59aa40a37698e04d30b08f1825cd
b3spectacled <modernbetamc@gmail.com>
2022-06-04 14:49:56 -0700

    Change height provider mixins to Injects.
    Addresses #34.

--- src/main/java/com/bespectacled/modernbeta/mixin/heightprovider/MixinBiasedToBottomHeightProvider.java
+++ src/main/java/com/bespectacled/modernbeta/mixin/heightprovider/MixinBiasedToBottomHeightProvider.java
@@ -1,16 +1,42 @@
 package com.bespectacled.modernbeta.mixin.heightprovider;
 
-import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import java.util.Random;
 
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.bespectacled.modernbeta.ModernBeta;
+
+import net.minecraft.world.gen.HeightContext;
+import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.heightprovider.BiasedToBottomHeightProvider;
 
 @Mixin(BiasedToBottomHeightProvider.class)
 public class MixinBiasedToBottomHeightProvider {
-    @Redirect(method = "get", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V"))
-    private void redirectWarn(Logger self, String string, Object object) {
-        // Suppress console spam, see: See: MC-236933 and MC-236723
+    @Unique private boolean warned = false;
+    
+    @Shadow private YOffset minOffset;
+    @Shadow private YOffset maxOffset;
+    @Shadow private int inner;
+    
+    @Inject(method = "get", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V")) 
+    private void injectWarnReturn(Random random, HeightContext context, CallbackInfoReturnable<Integer> info) {
+        int minOffsetY = this.minOffset.getY(context);
+        int maxOffsetY = this.maxOffset.getY(context);
+        
+        if (maxOffsetY - minOffsetY - this.inner + 1 <= 0) {
+            if (this.warned) {
+                info.setReturnValue(minOffsetY);
+            } else {
+                ModernBeta.log(Level.INFO, "Suppressing height provider warning..");
+                
+                this.warned = true;
+            }
+        }
     }
 }
