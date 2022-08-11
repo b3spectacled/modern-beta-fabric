98d6b1ec8fefd5799ca6c989fbc01f760ddcdc7d
b3spectacled <modernbetamc@gmail.com>
2022-08-10 23:20:32 -0700

    Fix issue with default noise post processor.

--- src/main/java/com/bespectacled/modernbeta/api/world/gen/noise/NoisePostProcessor.java
+++ src/main/java/com/bespectacled/modernbeta/api/world/gen/noise/NoisePostProcessor.java
@@ -1,7 +1,7 @@
 package com.bespectacled.modernbeta.api.world.gen.noise;
 
 public interface NoisePostProcessor {
-    public static final NoisePostProcessor DEFAULT = (noise, noiseX, noiseY, noiseZ) -> 0;
+    public static final NoisePostProcessor DEFAULT = (noise, noiseX, noiseY, noiseZ) -> noise;
     
     double sample(double noise, int noiseX, int noiseY, int noiseZ);
 }
